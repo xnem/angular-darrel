d0319074e4764aa5379e9b5a157df2b51e870f5c
xnem <oneofgm@gmail.com>
2022-04-11 18:52:55 +0900

    3rd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculateHistoryDetailComponent } from './pages/components/calculate-history-detail/calculate-history-detail.component';
+import { CalculateHistoryComponent } from './pages/components/calculate-history/calculate-history.component';
+import { CalculateComponent } from './pages/components/calculate/calculate.component';
+import { DriverMasterEditComponent } from './pages/components/driver-master-edit/driver-master-edit.component';
+import { DriverMasterViewComponent } from './pages/components/driver-master-view/driver-master-view.component';
+import { MenuComponent } from './pages/components/menu/menu.component';
+import { UrlConst } from './pages/constants/url-const';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: UrlConst.SLASH + UrlConst.PATH_MENU, pathMatch: 'full' },
+  { path: UrlConst.PATH_MENU, component: MenuComponent },
+  { path: UrlConst.PATH_DRIVER_MASTER, component: DriverMasterViewComponent},
+  { path: UrlConst.PATH_DRIVER_MASTER_EDIT, component: DriverMasterEditComponent},
+  { path: UrlConst.PATH_CALCULATE, component: CalculateComponent},
+  { path: UrlConst.PATH_CALCULATE_HISTORY, component: CalculateHistoryComponent },
+  { path: UrlConst.PATH_CALCULATE_HISTORY_DETAIL, component: CalculateHistoryDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoadingService } from './core/services/loading.service';
+import { RoutingService } from './core/services/routing.service';
+import { UrlConst } from './pages/constants/url-const';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'darrel-angular';
+
+  constructor(
+    private routingService: RoutingService,
+    private loadingService: LoadingService
+  ) { }
+
+  // アクセスしているURLからページ名を返却する
+  public getPageName(): String {
+
+    if (UrlConst.SLASH === this.routingService.router.url ||
+      UrlConst.SLASH + UrlConst.PATH_MENU === this.routingService.router.url) {
+      return 'メニュー';
+    } else if (UrlConst.SLASH + UrlConst.PATH_DRIVER_MASTER === this.routingService.router.url) {
+      return 'ドライバーマスタ　一覧';
+    } else if (UrlConst.SLASH + UrlConst.PATH_DRIVER_MASTER_EDIT === this.routingService.router.url) {
+      return 'ドライバーマスタ　編集';
+    } else if (UrlConst.SLASH + UrlConst.PATH_CALCULATE === this.routingService.router.url) {
+      return '料金計算';
+    } else if (UrlConst.SLASH + UrlConst.PATH_CALCULATE_HISTORY === this.routingService.router.url) {
+      return '料金計算履歴　一覧';
+    } else if (UrlConst.SLASH + UrlConst.PATH_CALCULATE_HISTORY_DETAIL === this.routingService.router.url) {
+      return '料金計算履歴　詳細';
+    }
+    return 'エラー';
+  }
+
+  // ヘッダーの家アイコンクリックでメニュー画面に遷移する
+  public navigateToMenu(): void {
+    this.loadingService.startLoading();
+    this.routingService.navigate("menu");
+    this.loadingService.stopLoading();
+  }
 }
